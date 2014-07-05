b74ee845541d9a2e33a175491758896c7eaa8e2d
sheldon xu <sheldon.xu@ymail.com>
2014-07-05 22:56:53 +0800

    add timer and refactor

--- bbxclient.py
+++ bbxclient.py
@@ -4,24 +4,34 @@ import wx, os
 
 class Frame(wx.Frame):
     
-    def __init__(self, image, parent=None):  
+    def __init__(self, parent=None):  
         wx.Frame.__init__(self, parent)  
         self.panel = wx.Panel(self)  
-        self.bmp = wx.StaticBitmap(parent=self.panel, bitmap=image.ConvertToBitmap())  
-	self.bmp.Bind(wx.EVT_LEFT_UP, self.onClick)
+	self.LoadImage()
+	self.bmp.Bind(wx.EVT_LEFT_UP, self.OnClick)
 
-    def onClick(self, event):
+	self.timer = wx.Timer(self)
+	self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
+	self.timer.Start(1000 * 60 * 5)
+
+    def LoadImage(self):
+        image = wx.Image('/etc/bobox/bobox.jpg', wx.BITMAP_TYPE_JPEG)  
+	image.Rescale(600, 1024)
+        self.bmp = wx.StaticBitmap(parent=self.panel, bitmap=image.ConvertToBitmap())  
+
+    def OnClick(self, event):
 	pos = event.GetPosition()
 	if pos.x <= 100 and pos.y <= 100:
             os.system("/usr/bin/onboard &")
             os.system("/usr/bin/nm-connection-editor &")
 
+    def OnTimer(self, event):
+	self.LoadImage()
+
 
 class App(wx.App):  
     def OnInit(self):  
-        image = wx.Image('/etc/bobox/bobox.jpg', wx.BITMAP_TYPE_JPEG)  
-	image.Rescale(600, 1024)
-        self.frame = Frame(image)  
+        self.frame = Frame()  
         self.frame.Show()  
         self.frame.ShowFullScreen(True)  
         self.SetTopWindow(self.frame)  
