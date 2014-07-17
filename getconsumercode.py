8e0cd1973e810a6a07275319978948bf00ee0a24
sheldon xu <sheldon.xu@ymail.com>
2014-07-17 17:19:15 +0800

    python script to get consumer code

---
+++ getconsumercode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import memcache, requests, time
+
+idfile = open('/etc/bobox/device_id', "r")
+try:
+    device_id = idfile.read().rstrip('\n')
+finally:
+    idfile.close()
+
+infurl = "http://www.lomotime.cn/admin/mims/reg.do"
+args = {"method": "getMachineConsumerCode", "machineCode": device_id}
+
+def get_consumer_code():
+    req = requests.get(infurl, params = args)
+    if req.status_code == 200:
+        shared = memcache.Client(['127.0.0.1:11211'], debug=0)
+        shared.set('ConsumerCode', req.text)
+
+while True:
+    get_consumer_code()
+    time.sleep(5)
+
