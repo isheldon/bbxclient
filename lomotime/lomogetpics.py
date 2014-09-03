261903900068cdbf8ce7547ad0ad194f7064846c
sheldon xu <sheldon.xu@ymail.com>
2014-09-03 14:01:22 +0800

    get pics: get qrcode and ad images

--- lomotime/lomogetpics.py
+++ lomotime/lomogetpics.py
@@ -7,22 +7,36 @@ import lomoconf
 machine_id = lomoconf.machine_id()
 base_imgurl = lomoconf.img_base_url()
 infurl = lomoconf.info_url()
-args = {"method": "getBackGroundPic", "machineCode": machine_id}
-
+bgargs = {"method": "getBackGroundPic", "machineCode": machine_id}
+adargs = {"method": "getAD", "machineCode": machine_id}
+interval = lomoconf.qrcode_interval()
 
 def get_pics():
-    req = requests.get(infurl, params = args)
+    # get background picture
+    req = requests.get(infurl, params = bgargs)
     if req.status_code == 200:
-        #print req.text
+        # print req.text
         jpic = json.loads(req.text)
-        dimen_pic = jpic["dimenPic"]
         back_pic = jpic["backPic"]
 
-        if dimen_pic != "null":
-	    cmd = "sudo wget " + base_imgurl + dimen_pic + " -q -O /etc/lomotime/qrcode.jpg"
-            os.system(cmd)
         if back_pic != "null":
 	    cmd = "sudo wget " + base_imgurl + back_pic + " -q -O /etc/lomotime/background.jpg"
+            os.system(cmd)
+
+    req = requests.get(infurl, params = adargs)
+    if req.status_code == 200:
+        # print req.text
+        jpic = json.loads(req.text)
+        dimen_pic = jpic["dimenCode"]
+        ad_pic = jpic["photoPrinting"]
+        # print dimen_pic
+        # print ad_pic
+
+        if dimen_pic != "null":
+	    cmd = "sudo wget " + dimen_pic + " -q -O /etc/lomotime/qrcode.jpg"
+            os.system(cmd)
+        if ad_pic != "null":
+	    cmd = "sudo wget " + ad_pic + " -q -O /etc/lomotime/ad.jpg"
             os.system(cmd)
 
 while True:
@@ -31,5 +45,5 @@ while True:
     except Exception, e:
       # ignore 
       print ""
-    time.sleep(60)
+    time.sleep(interval)
 
